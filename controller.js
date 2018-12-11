1a046c9a8c6f53b718f9e0cdcf4ee62d27405c1d
LuthfiArfi <luthfi.arfi@gmail.com>
2018-12-11 17:54:36 +0700

    commit

--- controller.js
+++ controller.js
@@ -1,1 +1,132 @@
+const Employee = require('./employeeModel.js');
+const Patient = require('./patientModel.js');
+const View = require('./view.js');
 
+
+class Controller {
+  // console.log('MASUK CONTROLLER');
+  static register(username, password, role){
+    var newUser = new Employee(username, password, role)
+    newUser.create(function(errCreate) {
+      if (errCreate) {
+        View.errorCreateUser(errCreate);
+      }
+      else {
+        Employee.findAll(function(errFindAll, dataAll) {
+          if (errFindAll) {
+            View.errorFind(errFindAll)
+          }
+          else {
+            var user ={
+              username : newUser.username,
+              password : newUser.password,
+              role : newUser.role
+            }
+            View.succesCreateUser(user,dataAll.length);
+          }
+        })
+      }
+    })
+  }
+
+  static login(username, password){
+    Employee.findAll(function(errFindAll,data) {
+      if (errFindAll) {
+        View.errorFind(errFindAll);
+      }
+      else {
+        // console.log(data)
+        Employee.findWhere('isLogin', 'true', function(errFindOne, dataOne) {
+          if (errFindOne) {
+            View.errorFind(errFindOne);
+          }
+          else {
+            if (dataOne.length == 0) {
+              var succLogin = false;
+              var userLogin = '';
+              for (var j = 0; j < data.length; j++) {
+                if (username == data[j].username && password == data[j].password){
+                  userLogin = data[j].username;
+                  Employee.updateIsLogin(data[j], 'true', function(errUpdate) {
+                    if (errUpdate) {
+                      View.errorUpdate(errUpdate);
+                    }
+                  })
+                  succLogin = true;
+                  break;
+                }
+                // console.log(data[i]);
+              }
+              if (succLogin == true) {
+                View.successLogin(userLogin);
+              }
+              else {
+                View.failLogin();
+              }
+            }
+            else {
+              View.somebodyIsLogin(dataOne[0].username);
+            }
+          }
+        })
+      }
+    });
+  }
+
+  static addPatient(name, diagnose){
+    Employee.findWhere('isLogin', 'true', function(errFindWhere, dataLogin) {
+      if (errFindWhere) {
+        View.errorFind(errFindWhere);
+      }
+      else {
+        if (dataLogin[0].role == 'dokter') {
+          var newPatient = new Patient(name, diagnose);
+          newPatient.create(function (errCreate) {
+            if (errCreate) {
+              View.failAddPatient(errCreate);
+            }
+            else {
+              Patient.findAll(function(errFindAll, dataAll) {
+                if (errFindAll) {
+                  View.errorFind(errFindAll)
+                }
+                else {
+                  View.succesCreatePatient(dataAll.length);
+                }
+              })
+            }
+          })
+        }
+        else {
+          View.notDoctor();
+        }
+      }
+    })
+  }
+
+  static logout(){
+    Employee.findWhere('isLogin', 'true', function(errFindWhere, dataLogin) {
+      if (errFindWhere) {
+        View.errorFind(errFindWhere);
+      }
+      else {
+        if (dataLogin.length > 0) {
+          var user = dataLogin[0].username;
+          Employee.updateIsLogin(dataLogin[0], 'false', function(errUpdate) {
+            if (errUpdate) {
+              View.errorUpdate(errUpdate);
+            }
+          })
+          View.successLogout(user);
+        }
+        else {
+          View.nobodyLogin();
+        }
+      }
+    })
+  }
+}
+
+
+
+module.exports = Controller;
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const argv = process.argv;
+const command = argv.slice(2);
+const Controller = require('./controller.js');
 
+switch (command[0]) {
+  case 'register':
+    Controller.register(command[1], command[2], command[3]);
+    break;
+  case 'login':
+    Controller.login(command[1], command[2]);
+    break;
+  case 'addPatient':
+    var commandSlice = command.slice(2);
+    var diagnose = commandSlice.join(' ');
+    Controller.addPatient(command[1], diagnose)
+    break;
+    default:
+  case 'logout':
+    Controller.logout();
+    break;
+}
