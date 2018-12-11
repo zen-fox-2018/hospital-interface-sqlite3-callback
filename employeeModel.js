a2c3a94a3ac7c4a5f0c1961e7ed0d8fa3dfd36fc
LuthfiArfi <luthfi.arfi@gmail.com>
2018-12-11 21:38:33 +0700

    commit

--- employeeModel.js
+++ employeeModel.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('hospital.db')
+const db = new sqlite3.Database('hospital.db');
 
 class Employee {
   // console.log('MASUK EMPLOYEE');
--- setup.js
+++ setup.js
@@ -1,3 +1,6 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('hospital.db');
+
 function setupEmployees(){
     var createSetup = `CREATE TABLE
                         Employees (
