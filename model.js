8eb6cb8d62b849add58785e35d0ebde191307188
irsyahms <irsyahmardiahs@gmail.com>
2018-12-11 10:28:39 +0700

    Add files via upload

---
+++ model.js
@@ -0,0 +1,16 @@
+class Patient {
+  constructor(id, name, diagnosis) {
+    this.id = id
+    this.name = name
+    this.diagnosis = diagnosis
+  }
+}
+
+class Employee {
+  constructor(name, position, username, password) {
+    this.name = name
+    this.position = position
+    this.username = username
+    this.password = password
+  }
+}
